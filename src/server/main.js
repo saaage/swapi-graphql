96f160707406c0cc2ac73c75d01b8fcdb6b631b0
Sage911 <stefan.age@outlook.com>
2017-12-22 19:48:56 -0600

    Enabled CORS and turned off graphiql interface

--- src/server/main.js
+++ src/server/main.js
@@ -9,6 +9,7 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import swapiSchema from '../schema';
+import cors from 'cors';
 
 const app = express();
 
@@ -17,9 +18,10 @@ app.all('/graphql', (req, res) => res.redirect('/'));
 
 app.use(
   '/',
+  cors(),
   graphqlHTTP(() => ({
     schema: swapiSchema,
-    graphiql: true,
+    graphiql: false
   })),
 );
 
